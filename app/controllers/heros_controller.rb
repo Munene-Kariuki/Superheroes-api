def53e13cf8d8aff587586b2254c1c7239f45c6a
Denis <denismunene999@gmail.com>
2022-10-07 16:36:26 +0300

    add hero controller actions

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,9 +1,22 @@
 class HerosController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found 
 
   def index 
-    render json: Hero.all 
+    render json: Hero.all, except: :powers
   end 
 
-  def 
+  def show
+    hero = find_hero 
+    render json: hero 
+  end
+
+  private 
+  def find_hero 
+    Hero.find(params[:id]) 
+  end
+
+  def render_not_found 
+    render json: {error: "Hero not found"}, status: :not_found 
+  end
 
 end
