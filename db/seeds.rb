cd8c10bb452e0f9d672640b36f90f3ba7a1647be
Denis <denismunene999@gmail.com>
2022-10-07 16:05:25 +0300

    add validations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#Heroes 
+h1 = Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+h2 = Hero.create(name: "Doreen Green", super_name: "Squirrel Girl") 
+h3 = Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+h4 = Hero.create(name: "Chris Hermsworth", super_name: "Thor")
+h5 = Hero.create(name: "Tony stark", super_name: "Iron man") 
+
+#Powers 
+p1 = Power.create(name: "Super strength", description: "gives the wielder super-human strengths")
+p2 = Power.create(name: "Flight", description: "gives the wielder the ability to fly through the skies at supersonic speed") 
+p3 = Power.create(name: "Speed", description: "gives the wielder super-human speed") 
+p4 = Power.create(name: "Lightning", description: "gives the wielder ability to summon lightning") 
+p5 = Power.create(name: "Space travel", description: "gives the wielder ability to travel through space.")
+
+#Heropower 
+Heropower.create(strength: "Strong", hero_id: h1.id, power_id: p1.id)
+Heropower.create(strength: "Strong", hero_id: h1.id, power_id: p2.id)
+Heropower.create(strength: "Strong", hero_id: h1.id, power_id: p5.id) 
+Heropower.create(strength: "Weak", hero_id: h2.id, power_id: p3.id)
+Heropower.create(strength: "Strong", hero_id: h3.id, power_id: p2.id)
+Heropower.create(strength: "Strong", hero_id: h4.id, power_id: p2.id)
+Heropower.create(strength: "Strong", hero_id: h4.id, power_id: p1.id)
+Heropower.create(strength: "Strong", hero_id: h4.id, power_id: p4.id)
+Heropower.create(strength: "Strong", hero_id: h4.id, power_id: p5.id) 
+Heropower.create(strength: "Strong", hero_id: h5.id, power_id: p2.id) 
+Heropower.create(strength: "Strong", hero_id: h3.id, power_id: p5.id)
