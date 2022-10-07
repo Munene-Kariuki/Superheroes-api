58391239ea1506bb56e5122f08312338bd7c7d0b
Denis <denismunene999@gmail.com>
2022-10-07 16:56:23 +0300

    add powers controller actions

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,38 @@
 class PowersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+
+  def index 
+    render json: Power.all 
+  end 
+
+  def show 
+    power = find_power 
+    render json: power 
+  end
+
+  def update 
+    power = find_power 
+    power.update!(power_params) 
+    render json: power, status: :accepted
+  end
+
+
+  private 
+  def find_power 
+    Power.find(params[:id]) 
+  end
+
+  def power_params 
+    params.permit(:name, :description) 
+  end
+
+  def render_not_found 
+    render json: {error: "Power not found"}, status: :not_found 
+  end 
+
+  def render_invalid_record(exception) 
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
 end
